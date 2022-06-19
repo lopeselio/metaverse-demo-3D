2704e3ee909c9f19216e3ba6001bdb306d93c4e8
lopeselio <lopeselio@gmail.com>
2022-06-19 16:17:45 +0530

    assets and glb

--- src/components/Plane.js
+++ src/components/Plane.js
@@ -2,15 +2,15 @@ import { TextureLoader } from 'three';
 import { useLoader } from '@react-three/fiber';
 
 // Import Assets
-import MetalMap from '../assets/MetalMap.png';
-import MetalNormalMap from '../assets/MetalNormalMap.png';
+import grass from '../assets/grass.jpeg';
+import grasstexture from '../assets/grasstexture.png';
 
 const Plane = () => {
-    const [surface, color] = useLoader(TextureLoader, [MetalNormalMap, MetalMap])
+    const [surface, color] = useLoader(TextureLoader, [grasstexture, grass])
     return (
         <mesh position={[0, 0, 0]}>
             <planeBufferGeometry attach="geometry" args={[50, 50]} />
-            <meshStandardMaterial color={"#404040"} />
+            <meshStandardMaterial map={color} normalMap={surface} />
         </mesh>
     );
 }
